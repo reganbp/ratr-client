6cf277349844e6c35ca88852ff4b59caca993a3c
reganbp <reganbp@gmail.com>
2018-08-09 15:29:21 -0400

    update config script to remove trailing /

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://floating-temple-84056.herokuapp.com/',
+  production: 'https://floating-temple-84056.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
